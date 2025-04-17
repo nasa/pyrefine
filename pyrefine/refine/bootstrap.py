266a6af6abc8cdfb9e51fd400ae9ce4b82407f6b
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2025-04-17 10:43:38 -0400

    Add option for extra args to refine bootstrap

--- pyrefine/refine/bootstrap.py
+++ pyrefine/refine/bootstrap.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 class RefineBootstrap:
-    def __init__(self, project_name, initial_complexity=100_000, initial_wall_spacing=None):
+    def __init__(self, project_name, initial_complexity=100_000, initial_wall_spacing=None, extra_args=""):
         """
         refine bootstrap driver to generate an initial mesh for refinement from a CSM file using ESP
 
@@ -19,6 +19,9 @@ class RefineBootstrap:
         self.initial_complexity = initial_complexity
         self.initial_wall_spacing = initial_wall_spacing
 
+        #: str: Additional arguments passed to bootstrap
+        self.extra_args = extra_args
+
     def run(self):
         """
         Run the bootstrap process. Expects serveESP to be in your PATH.
@@ -31,7 +34,7 @@ class RefineBootstrap:
 
         subprocess.run(
             f"serveESP -batch {self.project_name}.csm > esp.txt \
-        \nref bootstrap {self.project_name}.egads > bootstrap.txt \
+        \nref bootstrap {self.project_name}.egads {self.extra_args} > bootstrap.txt \
         \nmpiexec refmpi adapt {self.project_name}-vol.meshb \
         {spalding_option1} {self.initial_complexity} -s 10 {spalding_option2} \
         -x {self.project_name}01.meshb -x {self.project_name}01.lb8.ugrid > initialize.txt \
